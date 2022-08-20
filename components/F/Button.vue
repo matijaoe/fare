5a781c7cf1b4e5d4fb3d274f4be2c441412e897f
Matija Osrečki <matija.osrecki@gmail.com>
2022-08-20 22:13:57 +0200

    Build different button states

--- components/F/Button.vue
+++ components/F/Button.vue
@@ -1,49 +1,81 @@
 <script setup lang="ts">
 type Props = {
-  variant?: 'primary' | 'secondary' | 'tertiary' | 'outline' | 'danger' | 'success'
-  size: 'sm' | 'md'
+  label?: string
+  variant?: 'primary' | 'secondary' | 'subtle' | 'outline' | 'danger' | 'warning' | 'success'
+  size?: 'xs' | 'sm' | 'md' | 'lg'
+  block?: boolean
+  icon?: string
+  iconPlacement?: 'left' | 'right'
+  loading?: boolean
+  disabled?: boolean
 }
 
 const props = withDefaults(defineProps<Props>(), {
   variant: 'primary',
   size: 'md',
+  iconPlacement: 'left',
 })
 
 const variantStyle = computed(() => {
   switch (props.variant) {
     case 'primary':
-      return 'bg-stone-9 text-stone-1 hover:(bg-stone-8)'
+      return 'bg-stone-9 text-stone-1 hover:not-disabled:(bg-stone-8)'
     case 'secondary':
-      return 'bg-stone-2 text-stone-8 hover:(bg-stone-3/75 text-stone-9)'
-    case 'tertiary':
-      return 'bg-transparent text-stone-8 hover:(bg-stone-2/75 text-stone-9)'
+      return 'bg-stone-2 text-stone-9 hover:not-disabled:(bg-stone-3/75)'
+    case 'subtle':
+      return 'bg-transparent text-stone-9 hover:not-disabled:(bg-stone-2/75)'
     case 'outline':
-      return 'bg-transparent text-stone-8 !border-current hover:(bg-stone-2 text-stone-9)'
+      return 'bg-transparent text-stone-9 !border-current hover:not-disabled:(bg-stone-2)'
     case 'danger':
-      return 'bg-red-5/85 text-stone-8 !border-current hover:(bg-red-5/75)'
+      return 'bg-red-5/80 text-stone-9 !border-current hover:not-disabled:(bg-red-5/90)'
+    case 'warning':
+      return 'bg-amber-5/80 text-stone-9 !border-current hover:not-disabled:(bg-amber-5/90)'
     case 'success':
-      return 'bg-green-5/85 text-stone-8 !border-current hover:(bg-green-5/75)'
+      return 'bg-green-5/80 text-stone-9 !border-current hover:not-disabled:(bg-green-5/90)'
   }
 })
 
 const sizeStyle = computed(() => {
   switch (props.size) {
+    case 'xs':
+      return 'py-0.4 px-1 text-[0.6rem] gap-1 font-bold'
     case 'sm':
-      return 'py-1 px-2.5 text-xs'
+      return 'py-1 px-2.5 text-xs gap-1 font-bold'
     case 'md':
-      return 'py-1.75 px-5 text-sm'
+      return 'py-1.5 px-4 text-sm gap-2 font-bold'
+    case 'lg':
+      return 'py-2.5 px-7 text-base gap-3 font-bold'
   }
 })
+
+const widthStyle = computed(() => props.block ? 'w-full' : 'w-auto')
+const disabledStyle = computed(() => ({
+  'opacity-45': props.disabled && !props.loading,
+  'opacity-75': props.loading,
+}))
 </script>
 
 <template>
   <button
-    font="bold"
     border="1.5 transparent"
-    class="rounded-sm  uppercase disabled:(opacity-40 cursor-not-allowed)"
-    :class="[variantStyle, sizeStyle]"
+    class="rounded-sm  uppercase disabled:(cursor-not-allowed)"
+    :class="[widthStyle, variantStyle, sizeStyle, disabledStyle]"
+    flex
+    justify-center
+    items-center
+    text="center"
+    :disabled="loading || disabled"
   >
-    <slot />
+    <slot v-if="loading" name="loading">
+      <Icon class="i-tabler-loader-2 animate-spin" />
+    </slot>
+    <slot v-else-if="(icon && iconPlacement === 'left') || $slots.left" name="left">
+      <Icon :class="icon" />
+    </slot>
+    <slot>{{ label }}</slot>
+    <slot v-if="(icon && iconPlacement === 'right') || $slots.right" name="right">
+      <Icon :class="icon" />
+    </slot>
   </button>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -14,46 +14,156 @@
       gap-2
       items-start
     >
-      <FButton variant="primary">
-        play
-      </FButton>
-      <FButton variant="secondary">
-        calculate
-      </FButton>
-      <FButton variant="tertiary">
-        export
-      </FButton>
-      <FButton variant="outline">
-        info
-      </FButton>
-      <FButton variant="danger">
-        discard
-      </FButton>
-      <FButton variant="success">
-        apply
-      </FButton>
-      <!-- small -->
       <h1 mt-4>
-        Small buttons
+        Large
       </h1>
-      <FButton variant="primary" size="sm">
-        play
-      </FButton>
-      <FButton variant="secondary" size="sm">
-        calculate
-      </FButton>
-      <FButton variant="tertiary" size="sm">
-        export
-      </FButton>
-      <FButton variant="outline" size="sm">
-        info
-      </FButton>
-      <FButton variant="danger" size="sm">
-        discard
-      </FButton>
-      <FButton variant="success" size="sm">
-        apply
-      </FButton>
+      <!-- Large -->
+      <div flex gap-2 w-full>
+        <FButton
+          variant="primary"
+          size="lg"
+          icon="i-tabler-play"
+          icon-placement="right"
+        >
+          play
+        </FButton>
+        <FButton
+          variant="secondary"
+          size="lg"
+          block
+          icon="i-tabler-bluetooth-off"
+        >
+          disconnect
+        </FButton>
+      </div>
+
+      <div
+        grid
+        grid-cols-3
+        gap-4
+      >
+        <!-- Medium -->
+        <div
+          flex
+          flex-col
+          gap-2
+          items-start
+        >
+          <h1 mt-4>
+            Medium
+          </h1>
+          <FButton variant="primary" icon="i-tabler-cookie">
+            eat
+          </FButton>
+          <FButton
+            variant="secondary"
+            icon="i-tabler-edit"
+            icon-placement="right"
+          >
+            edit
+          </FButton>
+          <FButton variant="subtle">
+            <template #left>
+              <Icon i-tabler-archive />
+            </template>
+            archive
+          </FButton>
+          <FButton variant="outline" icon="i-tabler-info-circle">
+            info
+          </FButton>
+          <FButton variant="danger">
+            discard
+          </FButton>
+          <FButton variant="warning" icon="i-tabler-error-404">
+            warning
+          </FButton>
+          <FButton variant="success">
+            apply
+          </FButton>
+        </div>
+
+        <!-- small -->
+        <div
+          flex
+          flex-col
+          gap-2
+          items-start
+        >
+          <h1 mt-4>
+            Small
+          </h1>
+          <FButton variant="primary" size="sm" loading>
+            play
+          </FButton>
+          <FButton variant="secondary" size="sm" loading>
+            calculate
+          </FButton>
+          <FButton variant="subtle" size="sm" loading>
+            export
+          </FButton>
+          <FButton
+            variant="outline"
+            size="sm"
+            loading
+            icon="i-tabler-clipboard"
+          >
+            copy
+          </FButton>
+          <FButton
+            variant="danger"
+            size="sm"
+            loading
+          >
+            discard
+          </FButton>
+          <FButton variant="warning" size="sm" loading>
+            warning
+          </FButton>
+          <FButton variant="success" size="sm" loading>
+            apply
+          </FButton>
+        </div>
+
+        <div
+          flex
+          flex-col
+          gap-2
+          items-start
+        >
+          <!-- xtra small -->
+          <div
+            flex
+            flex-col
+            gap-2
+            items-start
+          >
+            <h1 mt-4>
+              XSmall
+            </h1>
+            <FButton variant="primary" size="xs">
+              play
+            </FButton>
+            <FButton variant="secondary" size="xs">
+              calculate
+            </FButton>
+            <FButton variant="subtle" size="xs">
+              export
+            </FButton>
+            <FButton variant="outline" size="xs" icon="i-tabler-clipboard">
+              copy
+            </FButton>
+            <FButton variant="danger" size="xs">
+              discard
+            </FButton>
+            <FButton variant="warning" size="xs">
+              warning
+            </FButton>
+            <FButton variant="success" size="xs">
+              apply
+            </FButton>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
