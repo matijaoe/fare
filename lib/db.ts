7695c51235d6b2a73c009e8608eb1f68edff5b7e
Matija Osrečki <matija.osrecki@gmail.com>
2022-11-05 00:34:50 +0100

    Try to fix prisma after build

--- lib/db.ts
+++ lib/db.ts
@@ -1,3 +1,21 @@
+// import { PrismaClient } from '@prisma/client'
+
+// export const db = new PrismaClient()
+
 import { PrismaClient } from '@prisma/client'
 
-export const db = new PrismaClient()
+declare global {
+  // allow global `var` declarations
+  // eslint-disable-next-line no-var
+  var db: PrismaClient | undefined
+}
+
+export const db
+  = global.db
+  || new PrismaClient({
+    log: ['query'],
+  })
+
+if (process.env.NODE_ENV !== 'production') {
+  global.db = db
+}
