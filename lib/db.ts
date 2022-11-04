6d0985ef2e09096a884bb447e5bab28dced28216
Matija Osrečki <matija.osrecki@gmail.com>
2022-11-05 00:57:56 +0100

    Update deps, fixes

--- lib/db.ts
+++ lib/db.ts
@@ -1,21 +1,3 @@
-// import { PrismaClient } from '@prisma/client'
-
-// export const db = new PrismaClient()
-
 import { PrismaClient } from '@prisma/client'
 
-declare global {
-  // allow global `var` declarations
-  // eslint-disable-next-line no-var
-  var db: PrismaClient | undefined
-}
-
-export const db
-  = global.db
-  || new PrismaClient({
-    log: ['query'],
-  })
-
-if (process.env.NODE_ENV !== 'production') {
-  global.db = db
-}
+export const db = new PrismaClient()
