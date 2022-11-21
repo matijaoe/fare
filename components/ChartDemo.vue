a6f46466111272ab00b821d33be36d940887f6fd
Matija Osrečki <matija.osrecki@gmail.com>
2022-11-21 13:52:11 +0100

    Add reactive month range switch for transaction totals charts

--- components/ChartDemo.vue
+++ components/ChartDemo.vue
@@ -1,21 +1,22 @@
 <script setup lang="ts">
-const tension = 0.5
+import { RadioGroup, RadioGroupOption } from '@headlessui/vue'
 
+const tension = 0.5
 const skipped = (ctx: any, value: any) => ctx.p0.skip || ctx.p1.skip ? value : undefined
 const down = (ctx: any, value: any) => ctx.p0.parsed.y > ctx.p1.parsed.y ? value : undefined
 
-// Dates
-const { data: totals } = useTransactionTotalMonthly()
+// TODO:
+const monthCount = useLocalStorage('fare-month-range', 3)
 
-const { labels, data: expensesData } = $(useMonthlyTotals(totals, 'Expense'))
-const { data: incomeData } = $(useMonthlyTotals(totals, 'Income'))
+// Dates
+const { labels, expenseTotals, incomeTotals } = $(useMonthlyTotals(useTransactionTotalMonthly().data, monthCount))
 
 const chartData = computed(() => ({
   labels,
   datasets: [
     {
       label: 'Expenses',
-      data: expensesData,
+      data: expenseTotals,
       borderColor: '#FF9494',
       backgroundColor: '#FF9494',
       segment: {
@@ -27,7 +28,7 @@ const chartData = computed(() => ({
     },
     {
       label: 'Income',
-      data: incomeData,
+      data: incomeTotals,
       borderColor: '#B6E388',
       backgroundColor: '#B6E388',
       segment: {
@@ -40,30 +41,46 @@ const chartData = computed(() => ({
   ],
 }))
 
-const chartOptions = { responsive: true, mantainAspectRatio: false }
+const chartOptions = { responsive: true, maintainAspectRatio: true }
+
+const monthCountOptions = [3, 6, 12, 24].map(value => ({ label: `${value} mo`, value }))
 </script>
 
 <template>
-  <div
-    flex
-    lg:flex-col
-    w-full
-    gap-8
-  >
-    <bar-chart
-      flex-1
-      w-full
-      :height="200"
-      :chart-options="chartOptions"
-      :chart-data="chartData"
-    />
-    <line-chart
-      flex-1
-      w-full
-      :height="200"
-      :chart-data="chartData"
-      :chart-options="chartOptions"
-    />
+  <div space-y-8>
+    <RadioGroup v-model="monthCount" flex justify-center>
+      <div flex gap-2 text-center>
+        <RadioGroupOption
+          v-for="option in monthCountOptions"
+          :key="option.value"
+          v-slot="{ checked }"
+          :value="option.value"
+        >
+          <FButton size="sm" :variant="checked ? 'primary' : 'secondary'">
+            {{ option.label }}
+          </FButton>
+        </RadioGroupOption>
+      </div>
+    </RadioGroup>
+
+    <div
+      flex="~ col 2xl:row gap-8"
+    >
+      <bar-chart
+        flex-1
+        w-full
+        :height="250"
+        :chart-options="chartOptions"
+        :chart-data="chartData"
+      />
+      <line-chart
+        flex-1
+        w-full
+        :height="250"
+        :chart-data="chartData"
+        :chart-options="chartOptions"
+      />
+    </div>
   </div>
 </template>
 
--- composables/monthly-totals.ts
+++ composables/monthly-totals.ts
@@ -3,10 +3,8 @@ import { format, isThisYear } from 'date-fns'
 import type { Ref } from 'nuxt/dist/app/compat/capi'
 import type { TransactionTotalMonthlyObject } from '~~/models/resources/transaction'
 
-export const useMonthlyTotals = (totals: Ref<TransactionTotalMonthlyObject> | Ref<undefined>, type: TransactionType) => {
-  const monthsRange = ref(6)
-
-  const months = $computed(() => Array.from({ length: monthsRange.value }, (_, i) => {
+export const useMonthlyTotals = (totals: Ref<TransactionTotalMonthlyObject | undefined>, monthCount: Ref<number>) => {
+  const months = $computed(() => Array.from({ length: monthCount.value }, (_, i) => {
     const date = new Date()
     date.setMonth(date.getMonth() - i)
     return date
@@ -17,32 +15,33 @@ export const useMonthlyTotals = (totals: Ref<TransactionTotalMonthlyObject> | Re
     date: format(date, 'yyyy-MM'),
   })))
 
-  const transactionsTotals = computed(() => {
+  const calcTransactionsTotals = (type: TransactionType) => {
     const arr = totals.value?.[type] ?? []
 
     const getTransactionsForMonth = (date: string) => arr.filter(t => t.date === date)
 
-    return graphLabeledDates.map((date) => {
-      const transactionsForMonth = getTransactionsForMonth(date.date) ?? { type, currency: 'EUR', total: 0 }
+    return graphLabeledDates.map(({ label, date }) => {
+      const transactionsForMonth = getTransactionsForMonth(date)
       const total = transactionsForMonth.reduce((acc, curr) => acc + curr.total, 0)
 
+      const { type, currency } = transactionsForMonth[0] ?? {}
       return {
-        ...date,
+        label,
+        date,
         total,
         type,
-        currency: transactionsForMonth[0]?.currency ?? 'EUR',
-
+        currency,
       }
     }) ?? []
-  })
+  }
 
-  const labels = computed(() => transactionsTotals.value.map(t => t.label))
-  const data = computed(() => transactionsTotals.value.map(t => t.total))
+  const expenseTotals = computed(() => calcTransactionsTotals('Expense').map(({ total }) => total))
+  const incomeTotals = computed(() => calcTransactionsTotals('Income').map(({ total }) => total))
 
   return {
     months,
-    transactionsTotals,
-    labels,
-    data,
+    labels: computed(() => graphLabeledDates.map(t => t.label)),
+    expenseTotals,
+    incomeTotals,
   }
 }
